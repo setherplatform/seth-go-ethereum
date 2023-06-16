e6c88557d6444a95642b1b44624b5832c40060f1
flaviusburca <flaviusburca@users.noreply.github.com>
2023-06-16 12:18:37 +0300

    initial commit

---
+++ p2p/netutil/netstat_nop.go
@@ -0,0 +1,10 @@
+//go:build !linux && !windows
+// +build !linux,!windows
+
+package netutil
+
+func UdpPortListeners(port int) (processes map[int]string, err error) {
+	processes = make(map[int]string)
+
+	return
+}
